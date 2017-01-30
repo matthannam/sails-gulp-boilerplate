df80820f7555b580e46d753d8b279fb1675b8df6
matthannam <mhannam@gmail.com>
2017-01-30 15:32:42 -0500

    Init commit

---
+++ api/controllers/HomepageController.js
@@ -0,0 +1,10 @@
+/**
+ * HomepageController
+ *
+ * @description :: Server-side logic for managing homepages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
